45362fa219f3100f64952f9e27f433dc524acd88
Albi Mehmeti <albimehmeti777@gmail.com>
2026-06-23 01:19:11 +0200

    add project and issue model relations and fillable fields

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Issue extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'project_id',
         'title',
@@ -35,5 +38,10 @@ class Issue extends Model
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'issue_tag');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'issue_user');
     }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
+        'owner_id',
         'name',
         'description',
         'start_date',
@@ -22,5 +27,10 @@ class Project extends Model
     public function issues(): HasMany
     {
         return $this->hasMany(Issue::class);
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
     }
 }
